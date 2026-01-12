d254416e763779cdd5431e64d0dda83ac8d77989
Hersh Krishna <hershkrishna@protonmail.ch>
2026-01-12 17:53:34 -0600

    Complete 2.4

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -10,6 +10,11 @@ variable (a b c d : ℝ)
 #check (min_le_right a b : min a b ≤ b)
 #check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
 
+--Took a bit to figure out these names
+#check (le_max_right a b : b ≤  max a b)
+#check (le_max_left a b : a ≤ max a b )
+#check (max_le: a ≤ c → b ≤ c → max a b ≤ c)
+
 example : min a b = min b a := by
   apply le_antisymm
   · show min a b ≤ min b a
@@ -31,6 +36,7 @@ example : min a b = min b a := by
   apply h
   apply h
 
+
 example : min a b = min b a := by
   apply le_antisymm
   repeat
@@ -38,18 +44,72 @@ example : min a b = min b a := by
     apply min_le_right
     apply min_le_left
 
+-- to flip the sign
+#check le_of_not_ge
+#check ge_iff_le
+
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  · apply le_min
+    · apply le_trans
+      apply min_le_left
+      apply min_le_left
+    apply le_min
+    · apply le_trans
+      apply min_le_left
+      apply min_le_right
+    apply min_le_right
+  apply le_min
+  · apply le_min
+    · apply min_le_left
+    apply le_trans
+    apply min_le_right
+    apply min_le_left
+  apply le_trans
+  apply min_le_right
+  apply min_le_right
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  · apply add_le_add
+    apply min_le_left
+    apply le_rfl
+  · apply add_le_add
+    apply min_le_right
+    apply le_rfl
+
+
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  exact aux a b c
+  have h : min (a + c) (b + c) = min (a + c) (b + c) - c + c := by
+    rw [sub_add_cancel]
+  rw [h]
+  apply add_le_add
+  rw [sub_eq_add_neg]
+  apply le_trans
+  apply aux
+  rw [add_neg_cancel_right, add_neg_cancel_right]
+  rfl
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := calc
+  |a| - |b| ≤ |a - b + b| - |b| := by rw [sub_add_cancel]
+  _ ≤ |a - b| + |b| - |b| := by
+    apply sub_le_sub_right
+    apply abs_add
+  _ ≤ |a - b| := by
+    rw [add_sub_cancel_right]
+
 end
 
 section
@@ -66,7 +126,15 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  · apply dvd_add
+    · apply dvd_mul_of_dvd_right
+      apply dvd_mul_right
+    rw [pow_two]
+    apply dvd_mul_left
+  apply dvd_mul_of_dvd_right
+  exact h
+
 end
 
 section
@@ -78,7 +146,10 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.dvd_antisymm
+  repeat
+    apply Nat.dvd_gcd
+    apply Nat.gcd_dvd_right
+    apply Nat.gcd_dvd_left
+
 end
-
-
