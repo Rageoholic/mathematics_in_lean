fb377c60dac53603ca2ea28bb9c964ddb1cf0256
Hersh Krishna <hershkrishna@protonmail.ch>
2026-01-12 18:45:28 -0600

    Where I'm leaving off with ch2 first attempt

--- MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
+++ MIL/C02_Basics/S05_Proving_Facts_about_Algebraic_Structures.lean
@@ -36,22 +36,75 @@ variable (x y z : α)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
 example : x ⊓ y = y ⊓ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply le_inf
+    · apply inf_le_right
+    apply inf_le_left
+
 
 example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := by
-  sorry
+  apply le_antisymm
+  · apply le_inf
+    · trans x ⊓ y
+      apply inf_le_left
+      apply inf_le_left
+    · apply le_inf
+      · trans x ⊓ y
+        apply inf_le_left
+        apply inf_le_right
+      · apply inf_le_right
+  · apply le_inf
+    apply le_inf
+    · apply inf_le_left
+    · trans y ⊓ z
+      apply inf_le_right
+      apply inf_le_left
+    · trans y ⊓ z
+      apply inf_le_right
+      apply inf_le_right
 
 example : x ⊔ y = y ⊔ x := by
-  sorry
+  apply le_antisymm
+  repeat
+    apply sup_le
+    · apply le_sup_right
+    · apply le_sup_left
 
 example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := by
-  sorry
+  apply le_antisymm
+  · apply sup_le
+    · apply sup_le
+      · apply le_sup_left
+      · trans y ⊔ z
+        apply le_sup_left
+        apply le_sup_right
+    · trans y ⊔ z
+      apply le_sup_right
+      apply le_sup_right
+  · apply sup_le
+    · trans x ⊔ y
+      · apply le_sup_left
+      · apply le_sup_left
+    · apply sup_le
+      · trans x ⊔ y
+        · apply le_sup_right
+        · apply le_sup_left
+      · apply le_sup_right
 
 theorem absorb1 : x ⊓ (x ⊔ y) = x := by
-  sorry
+  apply le_antisymm
+  · apply inf_le_left
+  · apply le_inf
+    · rfl
+    · apply le_sup_left
 
 theorem absorb2 : x ⊔ x ⊓ y = x := by
-  sorry
+  apply le_antisymm
+  · apply sup_le
+    · rfl
+    · apply inf_le_left
+  · apply le_sup_left
 
 end
 
@@ -70,6 +123,7 @@ variable {α : Type*} [Lattice α]
 variable (a b c : α)
 
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = x ⊓ y ⊔ x ⊓ z) : a ⊔ b ⊓ c = (a ⊔ b) ⊓ (a ⊔ c) := by
+
   sorry
 
 example (h : ∀ x y z : α, x ⊔ y ⊓ z = (x ⊔ y) ⊓ (x ⊔ z)) : a ⊓ (b ⊔ c) = a ⊓ b ⊔ a ⊓ c := by
@@ -109,4 +163,3 @@ example (x y : X) : 0 ≤ dist x y := by
   sorry
 
 end
-
